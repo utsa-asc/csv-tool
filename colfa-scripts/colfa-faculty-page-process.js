4edd10a272a3907aa75e2c6e4817e73372182e23
garza-utsa <john.garza@utsa.edu>
2023-02-21 14:06:40 -0600

    updated profile generation, squashed a few empty space bugs

--- colfa-scripts/colfa-faculty-page-process.js
+++ colfa-scripts/colfa-faculty-page-process.js
@@ -5,19 +5,20 @@ const {execSync} = require('child_process');
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 var fs = require('fs');
 var tasks = [];
-var writableStream = fs.createWriteStream("colfa/colfa-faculty-posted.csv");
+var writableStream = fs.createWriteStream("colfa/colfa-faculty-all-profiles-posted.csv");
 require('dotenv').config();
 /* defining some constants */
 const CAS_HOST = process.env.CAS_HOST;
 const CAS_PORT = process.env.CAS_PORT;
 const API_KEY = process.env.API_KEY;
-const PAYLOAD_DOCUMENT = fs.readFileSync("json/faculty-block-minimum.json");
+const PAYLOAD_DOCUMENT = fs.readFileSync("json/faculty-page-minimum.json");
+const POST_URI = "/api/v1/create";
 const stream = csv.format({quoteColumns: true});
 stream.pipe(writableStream);
-const POST_URI = "/api/v1/create";
-headerOutput = ["Last", "First", "LastFirst", "honorific", "Title", "Research", "Education", "Discipline", "Tag", "Email", "casURI", "casAssetID", "uuid", "Notes"];
+headerOutput = ["Last", "First", "LastFirst", "honorific", "Title", "Research", "Education", "Discipline", "Tag", "Email", "casURI", "casAssetID", "uuid", "Notes"]
 stream.write(headerOutput);
 writableStream.on("finish", function(){ console.log("DONE!"); });
+
 var protocol = http;
 if (CAS_PORT == 443) {
   protocol = https;
@@ -30,15 +31,18 @@ fs.createReadStream('colfa/colfa-faculty-problems.csv')
     var parsedData = {
       last: obj.Last,
       first: obj.First,
-      lastFirst: obj.LastFirst,
       title: obj.Title,
+      pageURI: obj.LastFirst,
       discipline: obj.Discipline,
       tag: obj.Tag,
       email: obj.Email,
-      casURI: obj.casURI,
       research: obj.Research,
-      education: obj.Education
+      education: obj.Education,
+      blockURI: obj.casURI,
+      blockID: obj.casAssetID,
+      uuid: obj.uuid
     }
+    console.log(parsedData);
     tasks.push(parsedData);
     // processEachTask(parsedData);
     // execSync('sleep 1'); // block process for 1 second.
@@ -62,85 +66,101 @@ fs.createReadStream('colfa/colfa-faculty-problems.csv')
   console.log("waiting for tasks");
 
   function processEachTask(task, callback) {
-    var name = task.last + "-" + task.first;
-    name = name.replace(" ", "-").toLowerCase();
+    var name = task.pageURI;
+    name = name.toLowerCase();
     let parentFolderPath = "faculty/_blocks/" + task.tag;
     let displayName = task.first + " " + task.last;
-    var casUri = task.casURI.replace(" ", "-");
-    console.log('CAS URI will be: ' + casUri);
+    let casUri = task.casURI;
     // prep our POST payload data
     var payload = JSON.parse(PAYLOAD_DOCUMENT);
     // update our JSON with this task's computed properties
-    let primaryDepartmentNode = {
-      "type": "text",
-      "identifier": "primaryDepartment",
-      "text": task.discipline
-    }
-    let emailNode = {
-      "type": "text",
-      "identifier": "email",
-      "text": task.email
-    }
-    let detailsNode = {
-      "type": "group",
-      "identifier": "details",
-      "structuredDataNodes": [
-        primaryDepartmentNode, emailNode
-      ]
-    }
 
-    let rawTitles = task.title.split(',');
-    if (Array.isArray(rawTitles)) {
-      rawTitles.map(innerTitle => {
-        detailsNode.structuredDataNodes.push({
-          "type": "text",
-          "identifier": "title",
-          "text": innerTitle.trim()
-        });
-      });
-    } else {
-      detailsNode.structuredDataNodes.push({
-        "type": "text",
-        "identifier": "title",
-        "text": task.title
-      });
-    }
+    //academic analytics base structured data objects
+    let sdn = [
+      {
+        "type": "group",
+        "identifier": "awards",
+        "structuredDataNodes": [
+          {
+            "type": "text",
+            "identifier": "awardInfo",
+            "text": "utsaDiscovery"
+          }
+        ]
+      },
+      {
+        "type": "group",
+        "identifier": "presentations",
+        "structuredDataNodes": [
+          {
+            "type": "text",
+            "identifier": "presentationsInfo",
+            "text": "utsaDiscovery"
+          }
+        ]
+      },
+      {
+        "type": "group",
+        "identifier": "grants",
+        "structuredDataNodes": [
+          {
+            "type": "text",
+            "identifier": "grantsInfo",
+            "text": "utsaDiscovery"
+          }
+        ]
+      },
+      {
+        "type": "group",
+        "identifier": "publications",
+        "structuredDataNodes": [
+          {
+            "type": "text",
+            "identifier": "publicationInfo",
+            "text": "utsaDiscovery"
+          }
+        ]
+      }
+    ]
 
-    //https://cms.lehgarza.family/entity/open.act?id=209e33aaac1600055b7549d118f4ca25&type=file
-    let imageNode = {
-      "type": "group",
-      "identifier": "image",
-      "structuredDataNodes": [
-        {
-          "type": "asset",
-          "identifier": "file",
-          "filePath": "faculty/headshots/_utsa-profile-placeholder-400x500.svg",
-          "assetType": "file"
-        },
-        {
-          "type": "text",
-          "identifier": "alt",
-          "text": displayName
-        },
-        {
-          "type": "text",
-          "identifier": "type",
-          "text": "No Link"
-        }
-      ]
+    let blockNode = {
+      "type": "asset",
+      "identifier": "block",
+      "blockPath": task.blockURI,
+      "assetType": "block"
     };
+    let uuidNode = {
+      "type": "text",
+      "identifier": "uuid",
+      "text": task.uuid
+    };
+    let researchNode = {
+      "type": "text",
+      "identifier": "researchInterests",
+      "text": task.research
+    };
+    let degreesNode = {
+      "type": "text",
+      "identifier": "degrees",
+      "text": task.education
+    };
+
+    sdn.push(blockNode);
+    sdn.push(uuidNode);
+    sdn.push(researchNode);
+    sdn.push(degreesNode);
 
     //update payload SDN with our new group
-    payload.asset.xhtmlDataDefinitionBlock.structuredData.structuredDataNodes = [detailsNode, imageNode];
+    payload.asset.page.structuredData.structuredDataNodes = sdn;
+
     //update our metadata
     let metadataNode = {
-      "displayName": displayName
+      "title": displayName
     };
-    payload.asset.xhtmlDataDefinitionBlock.metadata = metadataNode;
+    payload.asset.page.metadata = metadataNode;
     //update remaining computed properties
-    payload.asset.xhtmlDataDefinitionBlock.parentFolderPath = parentFolderPath;
-    payload.asset.xhtmlDataDefinitionBlock.tags = [{ "name" : task.tag }];
-    payload.asset.xhtmlDataDefinitionBlock.name = name;
+    payload.asset.page.tags = [{ "name" : task.tag }];
+    payload.asset.page.name = name;
 
     let postData = JSON.stringify(payload);
     console.log("computed JSON payload:");
@@ -172,10 +192,10 @@ fs.createReadStream('colfa/colfa-faculty-problems.csv')
         postResponse = postResponse + d;
         let responseObj = JSON.parse(d);
           process.stdout.write(d);
-          process.stdout.write('\t' + payload.asset.xhtmlDataDefinitionBlock.parentFolderPath + "\t" + payload.asset.xhtmlDataDefinitionBlock.name);
+          process.stdout.write('\t' + payload.asset.page.parentFolderPath + "\t" + payload.asset.page.name);
           process.stdout.write('\n');
           let assetID = responseObj.createdAssetId;
-          //["Last", "First", "Title", "Research", "Education", "Discipline", "Tag", "Email", "CAS-URI", "CAS-ASSET-ID"];
+          //["Last", "First", "Title", "Research", "Education", "Discipline", "Tag", "Email", "CAS-URI", "CAS-ASSET-ID", "uuid", "CAS-Page-URI", "CAS-Page-ID"];
           outputResult = [
             task.last,
             task.first,
@@ -185,8 +205,11 @@ fs.createReadStream('colfa/colfa-faculty-problems.csv')
             task.discipline,
             task.tag,
             task.email,
-            payload.asset.xhtmlDataDefinitionBlock.parentFolderPath + "/" + payload.asset.xhtmlDataDefinitionBlock.name,
-            assetID
+            task.blockURI,
+            task.blockID,
+            task.uuid,
+            payload.asset.page.parentFolderPath + "/" + payload.asset.page.name,
+            assetID,
           ];
           stream.write(outputResult);
       });
